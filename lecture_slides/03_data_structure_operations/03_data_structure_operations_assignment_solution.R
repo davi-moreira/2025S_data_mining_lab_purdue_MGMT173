cdcee9df2cd561813a0b328996ede3e077d4b7db
Davi Moreira <davi.moreira@gmail.com>
2025-01-08 21:38:39 -0500

    update

--- lecture_slides/03_data_structure_operations/03_data_structure_operations_assignment_solution.R
+++
@@ -1,9 +0,0 @@
-
-# Load the data
-data <- read.csv("tech_company_data.csv")
-
-# Create a new column: Efficiency Score = Tasks_Completed / Hours_Worked
-data$Efficiency_Score <- data$Tasks_Completed / data$Hours_Worked
-
-# Display the first few rows of the updated data
-head(data)
